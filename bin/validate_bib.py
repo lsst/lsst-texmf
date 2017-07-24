e67f496c289d7ba689d243faf5babb861e1dcf4a
Jonathan Sick <jsick@lsst.org>
2017-07-24 16:09:04 -0400

    Add Python-based bib validation for CI
    - bin/validate_bib.py attempts to parse bibliographies with pybtex and
      the latex+latin parser, just like the sphinxcontrib-bibtex workflow
      for Sphinx-based technotes.
    
    - Make target: make test-pybtex exercises the bin/validate_bib.py
      script, and is called from .travis.yml.
    
    We don't test the gaia livelink bibliography since it seems unusable.

---
+++ bin/validate_bib.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+"""Ensure that lsst-texmf bibliographies can be opened by pybtex [1].
+
+pybtex compatibility is required for Sphinx documentation workflows.  This also
+helps us discover issues like unescaped ``%`` signs and duplicate keys.
+
+[1] https://docs.pybtex.org/
+"""
+
+import argparse
+import os
+import sys
+
+from pybtex.database.input import bibtex
+import latexcodec  # noqa provides the latex+latin codec
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        'paths',
+        nargs='*',
+        help='Paths to bib files.'
+    )
+    args = parser.parse_args()
+
+    # Validate input paths
+    for bibpath in args.paths:
+        if not os.path.exists(bibpath):
+            print('Cannot find bib for testing: {}'.format(bibpath))
+            sys.exit(1)
+
+    # We use the latex+latin code with Sphinx workflows.
+    parser = bibtex.Parser('latex+latin')
+    for bibpath in args.paths:
+        print('Parsing {}'.format(bibpath))
+        parser.parse_file(bibpath)
+
+
+if __name__ == '__main__':
+    main()
