b16dfe332ea79cc8e0d375f4eca1e0ee270fdc2f
William O'Mullane <womullan@lsst.org>
2023-05-20 00:03:25 +0100

    Tim's comments

--- bin/validate_authors.py
+++ bin/validate_authors.py
@@ -15,8 +15,7 @@ def make_all(authordb):
 
 
 def main():
-    dbfile = os.path.normpath(
-        os.path.join(".", "etc", "authordb.yaml"))
+    dbfile = os.path.normpath(os.path.join(".", "etc", "authordb.yaml"))
 
     with open(dbfile, "r") as fh:
         authordb = yaml.safe_load(fh)
