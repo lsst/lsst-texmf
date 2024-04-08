549c51da3673ff8ee24de402bd5162582780a8bc
Tim Jenness <tjenness@lsst.org>
2024-04-08 15:28:16 -0700

    Enable bibtex in guide
    Co-authored-by: Jonathan Sick <jsick@lsst.org>

--- docs/conf.py
+++ docs/conf.py
@@ -4,3 +4,17 @@
 # see metadata.yaml in this repo for to update document-specific metadata
 
 from documenteer.conf.guide import *  # noqa
+
+
+# BibTeX configuration
+bibtex_bibfiles = [
+    '../texmf/bibtex/bib/lsst.bib',
+    '../texmf/bibtex/bib/books.bib',
+    '../texmf/bibtex/bib/lsst-dm.bib',
+    '../texmf/bibtex/bib/refs.bib',
+    '../texmf/bibtex/bib/refs_ads.bib',
+]
+
+# Feel free to experiment with other options
+bibtex_default_style = "lsst_aa"
+bibtex_reference_style = "author_year"
