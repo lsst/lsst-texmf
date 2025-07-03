843a42bc955e3d6fa199a4d7b7f167b4589068d8
William O'Mullane <womullan@lsst.org>
2025-07-03 19:50:25 -0400

    validate_authors now uses authordb.py

--- bin/validate_authors.py
+++ bin/validate_authors.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python
 """Ensure that lsst-texmf authors file is ok in case an entry was added."""
 
-import os
 import subprocess
-from typing import Any
 
 import yaml
+from authordb import AuthorDbYaml, load_authordb
 
 
-def make_all(authordb: dict[str, Any]) -> None:
+def make_all(authordb: AuthorDbYaml) -> None:
     """Go through all authors and put in one big file so we can test."""
     allauthors = []
-    for a in authordb["authors"]:
+    for a in authordb.authors:
         allauthors.append(a)
     with open("authors.yaml", "w") as file:
         yaml.dump(allauthors, file)
@@ -19,12 +18,9 @@ def make_all(authordb: dict[str, Any]) -> None:
 
 def main() -> None:
     """Run script."""
-    dbfile = os.path.normpath(os.path.join(".", "etc", "authordb.yaml"))
+    adb = load_authordb()
 
-    with open(dbfile) as fh:
-        authordb = yaml.safe_load(fh)
-
-    make_all(authordb)
+    make_all(adb)
     subprocess.call("bin/db2authors.py", shell=True)
 
 
